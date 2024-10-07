77311fce85d4abc8b3bfb2b1deb3a43f6fcfb843
Nguyen Quy Than <quythan306@gmail.com>
2024-10-07 15:33:14 +0700

    template_home_login

---
+++ BaiTapLon/src/main/java/com/BaiTapLon/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.BaiTapLon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "")
+public class HomeController {
+    
+    @GetMapping("/home")
+    public String HomeUser(){
+        return "HomeUser";
+    }
+
+}
